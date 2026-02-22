639625dcd2a89445fb98592da77de06e76df7ae8
alexerby <mail@alexandereriksson.se>
2026-02-22 11:21:22 +0100

    fix: move DLL/SO initialization to module level for Arrays

--- pygel/linalg/array.py
+++ pygel/linalg/array.py
@@ -1,6 +1,10 @@
 import ctypes
 import os
 from pathlib import Path
+from itertools import chain
+
+LIB_PATH = Path(__file__).parent.parent.parent / "build" / "libgel.so"
+_LIB = ctypes.CDLL(os.path.abspath(LIB_PATH))
 
 
 class CMetaArray(ctypes.Structure):
@@ -13,46 +17,89 @@ class CMetaArray(ctypes.Structure):
     ]
 
 
+# gel_array_create
+_LIB.gel_array_create.argtypes = [ctypes.c_size_t, ctypes.c_size_t]
+_LIB.gel_array_create.restype = ctypes.POINTER(CMetaArray)
+
+# gel_array_free
+_LIB.gel_array_free.argtypes = [ctypes.POINTER(CMetaArray)]
+_LIB.gel_array_free.restype = None
+
+# gel_array_fill_bulk
+_LIB.gel_array_fill_bulk.argtypes = [
+    ctypes.POINTER(CMetaArray),
+    ctypes.POINTER(ctypes.c_double),
+]
+_LIB.gel_array_fill_bulk.restype = ctypes.c_int
+
+# gel_array_get
+_LIB.gel_array_get.argtypes = [
+    ctypes.POINTER(CMetaArray),
+    ctypes.c_size_t,
+    ctypes.c_size_t,
+    ctypes.POINTER(ctypes.c_double),
+]
+_LIB.gel_array_get.restype = ctypes.c_int
+
+
 class Array:
-    def __init__(self, data=None, lib_path=None):
+    def __init__(self, data=None):
 
-        if lib_path is None:
-            current_dir = Path(__file__).parent.resolve()
-            project_root = current_dir.parent.parent
-            lib_path = project_root / "build" / "libgel.so"
+        if data is None:
+            raise ValueError("Data required for initialization.")
 
-        self._lib = ctypes.CDLL(os.path.abspath(lib_path))
+        rows = len(data)
+        cols = len(data[0]) if rows > 0 else 0
+        total_size = rows * cols
 
-        if data is not None:
-            rows = len(data)
-            cols = len(data[0]) if rows > 0 else 0
-
-        # Setup argtypes/restypes
-        self._setup_ctypes()
-
-        # Constructor (C Allocation)
-        self._ptr = self._lib.gel_array_create(rows, cols)
+        # Constructor for the C Allocation
+        self._ptr = _LIB.gel_array_create(rows, cols)
         if not self._ptr:
             raise MemoryError("C-level Array allocation failed.")
 
-        if data is not None:
-            flat = [float(item) for row in data for item in row]
-            self.fill(flat)
+        flat_data = chain.from_iterable(data)
+        buffer = (ctypes.c_double * total_size)(*flat_data)
+
+        _LIB.gel_array_fill_bulk(self._ptr, buffer)
 
     @property
-    def shape(self):
+    def shape(self) -> tuple[int, int]:
         return (self._ptr.contents.rows, self._ptr.contents.cols)
 
-    def set_value(self, r, c, val):
-        self._lib.gel_array_set.argtypes = [
+    def set_value(self, r: int, c: int, val: float):
+        """Sets a double-precision value at the specified row and column.
+
+        Args:
+            r: row.
+            c: column.
+
+        Returns:
+            int: 0 on success, -1 if indices are out of bounds.
+        """
+
+        _LIB.gel_array_set.argtypes = [
             ctypes.POINTER(CMetaArray),
             ctypes.c_size_t,
             ctypes.c_size_t,
             ctypes.c_double,
         ]
-        return self._lib.gel_array_set(self._ptr, r, c, val)
 
-    def fill(self, flat_list):
+        return _LIB.gel_array_set(self._ptr, r, c, val)
+
+    def fill(self, flat_list: list | list[list]):
+        """Populates the array with data from a flattened list.
+
+        Args:
+            flat_list: A list of floats with length equal to rows * cols.
+
+        Returns:
+            int: 0 on success, -1 if any pointer is NULL.
+
+        Raises:
+            ValueError: If the list size does not match array capacity.
+            RuntimeError: If the C-level memcpy() fails
+        """
+
         expected_size = self.shape[0] * self.shape[1]
         if len(flat_list) != expected_size:
             raise ValueError(
@@ -63,7 +110,7 @@ class Array:
         DoubleArray = ctypes.c_double * expected_size
         buffer = DoubleArray(*flat_list)
 
-        status = self._lib.gel_array_fill_bulk(self._ptr, buffer)
+        status = _LIB.gel_array_fill_bulk(self._ptr, buffer)
 
         if status != 0:
             raise RuntimeError("C-level gel_array_fill_bulk failed.")
@@ -71,19 +118,19 @@ class Array:
     def __del__(self):
         """Ensure we don't get any leak memory in C."""
         if hasattr(self, "_ptr") and self._ptr:
-            self._lib.gel_array_free(self._ptr)
+            _LIB.gel_array_free(self._ptr)
 
     def __repr__(self):
         r, c = self.shape
         return f"GEL Array({r}x{c})."
 
     def __getitem__(self, index):
-        # Support array[r, c]
+        """Support array[r, c]."""
         r, c = index
         return self.get_value(r, c)
 
     def __setitem__(self, index, value):
-        # Support array[r, c] = val
+        """Support array[r, c] = val."""
         r, c = index
         status = self.set_value(r, c, float(value))
         if status != 0:
@@ -97,34 +144,9 @@ class Array:
             res.append(f"[{row_str}]")
         return "\n".join(res)
 
-    def _setup_ctypes(self):
-        # gel_array_create
-        self._lib.gel_array_create.argtypes = [ctypes.c_size_t, ctypes.c_size_t]
-        self._lib.gel_array_create.restype = ctypes.POINTER(CMetaArray)
-
-        # gel_array_free
-        self._lib.gel_array_free.argtypes = [ctypes.POINTER(CMetaArray)]
-        self._lib.gel_array_free.restype = None
-
-        # gel_array_fill_bulk
-        self._lib.gel_array_fill_bulk.argtypes = [
-            ctypes.POINTER(CMetaArray),
-            ctypes.POINTER(ctypes.c_double),
-        ]
-        self._lib.gel_array_fill_bulk.restype = ctypes.c_int
-
-        # gel_array_get
-        self._lib.gel_array_get.argtypes = [
-            ctypes.POINTER(CMetaArray),
-            ctypes.c_size_t,
-            ctypes.c_size_t,
-            ctypes.POINTER(ctypes.c_double),
-        ]
-        self._lib.gel_array_get.restype = ctypes.c_int
-
     def get_value(self, r, c):
         out = ctypes.c_double()
-        status = self._lib.gel_array_get(self._ptr, r, c, ctypes.byref(out))
+        status = _LIB.gel_array_get(self._ptr, r, c, ctypes.byref(out))
         if status != 0:
             raise IndexError(f"Indices ({r}, {c}) out of bounds.")
         return out.value
